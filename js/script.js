d22c9ea9b2e9e72184e2879addd869f0b857371d
Luca <lu.cucchi@gmail.com>
2020-02-28 17:20:58 +0100

    fix jq

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 $('.dropdown-menu > a').click(function(){
     $('.dropdown').hide();
     $(this).siblings('ul').slideDown(500);
+    $('.dropdown-menu > a').removeClass('active');
     $(this).addClass('active');
 });
 
 $('.dropdown').mouseleave(function(){
-    $(this).hide(500);
+    $(this).hide(600);
     $('.dropdown-menu > a').removeClass('active');
 });
