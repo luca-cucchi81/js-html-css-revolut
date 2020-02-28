6dac0487ccc8fe9f0c1c534a05e213864b631921
Luca <lu.cucchi@gmail.com>
2020-02-28 17:15:05 +0100

    nav style + jq (finito)

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
+$('.dropdown-menu > a').click(function(){
+    $('.dropdown').hide();
+    $(this).siblings('ul').show(500);
+    $(this).addClass('active');
+});
 
+$('.dropdown').mouseleave(function(){
+    $(this).hide(500);
+    $('.dropdown-menu > a').removeClass('active');
+});
