3b392525beaedf3cecefca35ead45d3810bf72cb
Luca <lu.cucchi@gmail.com>
2020-02-29 15:36:45 +0100

    fix

--- js/script.js
+++ js/script.js
@@ -9,23 +9,22 @@ $('.dropdown').mouseleave(function(){
     $(this).hide(600);
     $('.dropdown-menu > a').removeClass('active');
 });
-
-$('#trigger').mouseenter(function(){
-    $('#trigger').siblings().fadeTo(50, 0);
+// jquery per scelta piani tariffari
+$('#trigger').hover(function(){
+    $('#trigger').siblings().fadeTo(1, 0);
 });
 
 $('#trigger').mouseleave(function(){
-    $('#trigger').siblings().fadeTo(50, 1);
+    $('#trigger').siblings().fadeTo(1, 1);
 });
 
-// jquery per scelta piani tariffari
-$('#trigger').siblings().mouseenter(function(){
-    $('#trigger').fadeTo(50, 0);
-    $('#trigger').siblings().fadeTo(50, 0);
-    $(this).fadeTo(0, 1)
+$('#trigger').siblings().hover(function(){
+    $('#trigger').fadeTo(1, 0);
+    $('#trigger').siblings().fadeTo(1, 0);
+    $(this).fadeTo(1, 1)
 })
 
 $('#trigger').siblings().mouseleave(function(){
-    $('#trigger').fadeTo(0, 1);
-    $('#trigger').siblings().fadeTo(0, 1);
+    $('#trigger').fadeTo(1, 1);
+    $('#trigger').siblings().fadeTo(1, 1);
 })
